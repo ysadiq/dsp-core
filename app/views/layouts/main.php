e6c7f8a99d79df05fe05b73aebe61b4eb5694cb8
Lee Hicks <leehicks@dreamfactory.com>
2014-02-28 09:27:26 -0500

    fix chrome bug, no comments allowed in 'if' directives.

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -15,8 +15,8 @@
 	<!-- DreamFactory Typography Css -->
 	<link rel="stylesheet" href="/css/df-custom-bs.css">
 	<link rel="stylesheet" href="/css/forms.css">
-	<!--[if lt IE 9]>
 	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
 	<script src="/js/html5shiv.js"></script>
 	<script src="/js/respond.min.js"></script>
 	<![endif]-->
