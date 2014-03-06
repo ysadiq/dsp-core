91fc73078cd27e1677d029815d4ce90fd7ed0294
Jerry Ablan <jerryablan@dreamfactory.com>
2014-03-05 20:21:57 -0500

    Wednesday work

--- web/index.php
+++ web/index.php
@@ -27,7 +27,7 @@ use DreamFactory\Yii\Utility\Pii;
 /**
  * @type bool If true, your logs will grow large.
  */
-const DSP_DEBUG = true;
+const DSP_DEBUG = false;
 
 //	Load up composer...
 $_autoloader = require_once( __DIR__ . '/../vendor/autoload.php' );
