1aca4fbc9ef9f3564a22b3eab3de1eeb0f35d96e
Jerry Ablan <jerryablan@dreamfactory.com>
2014-04-09 12:50:44 -0400

    Disabled logging of all events by default

--- config/common.config.php
+++ config/common.config.php
@@ -201,7 +201,7 @@ return array_merge(
         'dsp.enable_event_scripts' => true,
         //  If true, events that have been dispatched to a handler are written to the log
         'dsp.log_events' => true,
-        // If true, ALL events (with or without handlers) are written to the log. Trumps dsp.log_events.
+        // If true, ALL events (with or without handlers) are written to the log. Trumps dsp.log_events. Be aware that enabling this can and will impact performance negatively.
         'dsp.log_all_events' => false,
         //-------------------------------------------------------------------------
         //	Login Form Settings
