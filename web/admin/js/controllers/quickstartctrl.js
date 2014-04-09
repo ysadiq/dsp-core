434e8db9ba38deb312daf714d392730b0217de24
Jason Sykes <jasonsykes@dreamfactory.com>
2014-04-09 18:15:39 -0400

    Adding SDK, link to examples

--- web/admin/js/controllers/quickstartctrl.js
+++ web/admin/js/controllers/quickstartctrl.js
@@ -1,54 +1,58 @@
 var QuickStartCtrl = function ($scope, App, Config, Service, $location) {
-    setCurrentApp('getting_started');
-    Scope = $scope;
 
+    (function () {
+        setCurrentApp('getting_started');
+        $scope.Config = Config.get(function(data){
+            $scope.CORSConfig = {"id": data.id, "verbs":["GET","POST","PUT","MERGE","PATCH","DELETE","COPY"],"host":"*","is_enabled":true};
+        })
 
-    Scope.Config = Config.get(function(data){
-        Scope.CORSConfig = {"id": data.id, "verbs":["GET","POST","PUT","MERGE","PATCH","DELETE","COPY"],"host":"*","is_enabled":true};
-    })
+        $scope.Services = Service.get(function(){
 
-    Scope.Services = Service.get(function(){
+            $scope.Services.record.forEach(function(service){
+                if (service.type.indexOf("Local File Storage") != -1){
+                    $scope.defaultStorageID = service.id;
+                    $scope.defaultStorageName = service.api_name;
+                }
 
-        Scope.Services.record.forEach(function(service){
-            if (service.type.indexOf("Local File Storage") != -1){
-                Scope.defaultStorageID = service.id;
-                Scope.defaultStorageName = service.api_name;
-            }
-
+            });
         });
-    });
-    Scope.app ={native:0};
-    Scope.step = 1;
-    Scope.setStep = function(step){
-        if(step == 2 && Scope.app.native ==1){
-            Scope.step = 4;
-            Scope.create();
+        $scope.app ={native:0};
+        $scope.step = 1;
+
+    }());
+
+
+
+    $scope.setStep = function(step){
+        if(step == 2 && $scope.app.native ==1){
+            $scope.step = 4;
+            $scope.create();
             return;
         }else if(step == 3){
-            Scope.step = 4;
+            $scope.step = 4;
             return;
         }
 
-        Scope.step = step;
+        $scope.step = step;
     }
-    Scope.launchApp = function(){
-        window.open(location.protocol + '//' + location.host + '/' + Scope.defaultStorageName +'/applications/' + Scope.app.api_name+ '/index.html', "df-new");
+    $scope.launchApp = function(){
+        window.open(location.protocol + '//' + location.host + '/' + $scope.defaultStorageName +'/applications/' + $scope.app.api_name+ '/index.html', "df-new");
     }
-    Scope.downloadSDK = function(){
-        $("#sdk-download").attr('src', location.protocol + '//' + location.host + '/rest/system/app/' + Scope.app.id + '?sdk=true&app_name=admin')
+    $scope.downloadSDK = function(){
+        $("#sdk-download").attr('src', location.protocol + '//' + location.host + '/rest/system/app/' + $scope.app.id + '?sdk=true&app_name=admin')
     }
-    Scope.goToApps = function(){
+    $scope.goToApps = function(){
         $location.path('/app');
     };
-    Scope.goToDocs = function(){
+    $scope.goToDocs = function(){
         $location.path('/api');
     };
-    Scope.saveConfig = function () {
-        if(Scope.app.storage_service_id != 0){
+    $scope.saveConfig = function () {
+        if($scope.app.storage_service_id != 0){
             return;
         }
 
-        Config.update(Scope.CORSConfig, function () {
+        Config.update($scope.CORSConfig, function () {
 
             },
             function (response) {
@@ -63,56 +67,36 @@ var QuickStartCtrl = function ($scope, App, Config, Service, $location) {
 
             });
     };
-    Scope.create = function () {
+    $scope.create = function () {
 
-        if (Scope.app.native == '1') {
-            Scope.app.storage_service_id = null;
-            Scope.app.storage_container = null;
-            Scope.app.launch_url = "";
+        if ($scope.app.native == '1') {
+            $scope.app.storage_service_id = null;
+            $scope.app.storage_container = null;
+            $scope.app.launch_url = "";
 
 
-        }else if(Scope.app.storage_service_id == 0){
-            Scope.app.storage_service_id = null;
-            Scope.app.storage_container = null;
+        }else if($scope.app.storage_service_id == 0){
+            $scope.app.storage_service_id = null;
+            $scope.app.storage_container = null;
 
         }else{
-            Scope.app.storage_service_id = Scope.defaultStorageID;
-            Scope.app.storage_container = "applications"
+            $scope.app.storage_service_id = $scope.defaultStorageID;
+            $scope.app.storage_container = "applications"
         }
-        Scope.app.name = Scope.app.api_name;
-        App.save(Scope.app, function (data) {
-                //Scope.Apps.record.push(data);
-                Scope.app.id = data.id;
-                //Scope.app = data;
+        $scope.app.name = $scope.app.api_name;
+        App.save($scope.app, function (data) {
+                //$scope.Apps.record.push(data);
+                $scope.app.id = data.id;
+                //$scope.app = data;
                 if(window.top.Actions){
                     window.top.Actions.updateSession("update");
                 }
                 $.pnotify({
-                    title: Scope.app.api_name,
+                    title: $scope.app.api_name,
                     type: 'success',
                     text: 'Created Successfully'
                 });
                 Service.newApp = data;
-            },
-            function (response) {
-                Scope.setStep(1);
-                var code = response.status;
-                if (code == 401) {
-                    if(window.top.Actions){
-                        window.top.Actions.doSignInDialog("stay");
-                        return;
-                    }
-                }
-                $.pnotify({
-                    title: 'Error',
-                    type: 'error',
-                    hide: false,
-                    addclass: "stack-bottomright",
-                    text: getErrorString(response)
-                });
-
-
-
             });
 
 
