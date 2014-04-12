44bf0223f6d3334135b81dfa22bd1cfffc9df6dd
Jerry Ablan <jerryablan@dreamfactory.com>
2014-04-12 17:03:01 -0400

    Restoring event stuff yanked from remote-login branch

--- web/admin/js/directives/AdminApp.apisdk.js
+++ web/admin/js/directives/AdminApp.apisdk.js
@@ -1,141 +1,161 @@
+/**
+ * This file is part of the DreamFactory Services Platform(tm) (DSP)
+ *
+ * DreamFactory Services Platform(tm) <http://github.com/dreamfactorysoftware/dsp-core>
+ * Copyright 2012-2014 DreamFactory Software, Inc. <support@dreamfactory.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 'use strict';
 
-angular.module('AdminApp.apisdk', []).
-    directive('apisdk', ['$window', function($window) {
+angular.module( 'AdminApp.apisdk', [] ).directive(
+	'apisdk', [
+		'$window', function( $window ) {
+			return {
+				restrict:    'E',
+				replace:     true,
+				controller:  function( $scope, $element ) {
 
-        return {
-            restrict: 'E',
-            replace:true,
-            controller: function($scope, $element) {
+					$scope.activeTab = 'swagger';
 
+					$scope.init = function() {
+						$scope.activateTab( 'swagger' );
+						$scope.getCurrentServer();
+					};
 
-                $scope.activeTab = 'swagger';
+					$scope.activateTab = function( tabIdStr ) {
 
+						$scope.activeTab = tabIdStr;
+						$scope.$broadcast( 'tab:activate:tab', tabIdStr );
+					};
 
-                $scope.init = function() {
-                  $scope.activateTab('swagger');
-                    $scope.getCurrentServer();
-                };
+					$scope.getActiveTab = function() {
+						return $scope.activeTab;
+					};
 
+					$scope.getCurrentServer = function() {
 
+						return $window.location.protocol + '\/\/' + $window.location.host;
+					};
 
-                $scope.activateTab = function(tabIdStr) {
+					$scope.$on(
+						'tab:active', function( e ) {
 
-                    $scope.activeTab = tabIdStr;
-                    $scope.$broadcast('tab:activate:tab', tabIdStr);
-                };
+							$scope.$broadcast( 'tab:active:tab', $scope.getActiveTab() );
+						}
+					);
 
-                $scope.getActiveTab = function() {
-                    return $scope.activeTab;
-                };
+					$scope.$on(
+						'swagger:getServer', function( e ) {
 
+							$scope.$broadcast( 'swagger:getServer:address', $scope.getCurrentServer() );
+						}
+					);
 
-                $scope.getCurrentServer = function() {
+					$scope.$on(
+						'sdk:getServer', function( e ) {
 
-                    return $window.location.protocol + '\/\/' + $window.location.host;
-                };
+							$scope.$broadcast( 'sdk:getServer:address', $scope.getCurrentServer() );
+						}
+					);
 
+					$scope.init();
 
+				},
+				templateUrl: 'views/directives/apisdk/main.html',
+				link:        function( scope, elem, attrs ) {
 
+				}
+			}
+		}
+	]
+).directive(
+	'swagger', [
+		'$window', function( $window ) {
 
+			return {
+				restrict:    'E',
+				require:     '^?apisdk',
+				replace:     true,
+				scope:       {},
+				templateUrl: 'views/directives/apisdk/swagger.html',
+				link:        function( scope, elem, attrs ) {
 
+					scope.active = false;
+					scope.id = 'swagger';
+					scope.iframeUrl = null;
 
-                $scope.$on('tab:active', function(e) {
+					scope.init = function() {
 
-                    $scope.$broadcast('tab:active:tab', $scope.getActiveTab());
-                });
+						if ( attrs.standAlone === 'true' ) {
 
+							scope.iframeUrl = scope.getCurrentServer() + '/swagger/'
+						}
+						else {
 
-                $scope.$on('swagger:getServer', function(e) {
+							scope.$emit( 'tab:active' );
+							scope.$emit( 'swagger:getServer' );
+						}
+					};
 
-                    $scope.$broadcast('swagger:getServer:address', $scope.getCurrentServer());
-                });
+					scope.getCurrentServer = function() {
 
-                $scope.$on('sdk:getServer', function(e) {
+						return $window.location.protocol + '\/\/' + $window.location.host;
+					};
 
-                    $scope.$broadcast('sdk:getServer:address', $scope.getCurrentServer());
-                });
+					scope.$on(
+						'swagger:on', function( e, serviceNameStr ) {
 
+							if ( serviceNameStr ) {
+								scope.iframeUrl = scope.getCurrentServer() + '/swagger/#!/' + serviceNameStr;
+							}
 
+							scope.active = true;
+						}
+					);
 
-                $scope.init();
+					scope.$on(
+						'swagger:off', function( e ) {
 
+							scope.active = false;
+							scope.iframeUrl = null;
+						}
+					);
 
-            },
-            templateUrl: 'views/directives/apisdk/main.html',
-            link: function(scope, elem, attrs) {
+					scope.$on(
+						'tab:activate:tab', function( e, tabIdStr ) {
 
+							scope.active = scope.id === tabIdStr;
 
-            }
-        }
-    }])
-    .directive('swagger', ['$window', function($window) {
+							if ( scope.active && !scope.iframeUrl ) {
+								scope.$emit( 'swagger:getServer' );
+							}
+						}
+					);
 
-        return {
-            restrict: 'E',
-            require: '^?apisdk',
-            replace: true,
-            scope: {},
-            templateUrl: 'views/directives/apisdk/swagger.html',
-            link: function(scope, elem, attrs) {
+					scope.$on(
+						'tab:active:tab', function( e, tabIdStr ) {
 
-                scope.active = false;
-                scope.id = 'swagger';
-                scope.iframeUrl = null;
+							scope.active = scope.id === tabIdStr;
+						}
+					);
 
-                scope.init = function() {
+					scope.$on(
+						'swagger:getServer:address', function( e, addressStr ) {
 
-
-                    if (attrs.standAlone === 'true') {
-
-                        scope.iframeUrl = scope.getCurrentServer() + '/swagger/'
-                    }else {
-
-                        scope.$emit('tab:active');
-                        scope.$emit('swagger:getServer');
-                    }
-                };
-
-
-                scope.getCurrentServer = function() {
-
-                    return $window.location.protocol + '\/\/' + $window.location.host;
-                };
-
-                scope.$on('swagger:on', function(e, serviceNameStr) {
-
-
-                    if (serviceNameStr) {
-                        scope.iframeUrl = scope.getCurrentServer() + '/swagger/#!/' + serviceNameStr;
-                    }
-
-                    scope.active = true;
-                });
-
-                scope.$on('swagger:off', function(e) {
-
-                    scope.active = false;
-                    scope.iframeUrl = null;
-                });
-
-                scope.$on('tab:activate:tab', function(e, tabIdStr) {
-
-                    scope.active = scope.id === tabIdStr;
-
-                    if (scope.active && !scope.iframeUrl) {
-                        scope.$emit('swagger:getServer');
-                    }
-                });
-
-                scope.$on('tab:active:tab', function(e, tabIdStr) {
-
-                    scope.active = scope.id === tabIdStr;
-                });
-
-                scope.$on('swagger:getServer:address', function(e, addressStr) {
-
-                    scope.iframeUrl = addressStr + '/swagger/';
-                });
+							scope.iframeUrl = addressStr + '/swagger/';
+						}
+					);
 //                var swaggerIframe = $("#swaggerFrame");
 //                var swaggerDiv = $('#swagger');
 //                var docsIframe = $('#docsFrame');
@@ -150,57 +170,64 @@ angular.module('AdminApp.apisdk', []).
 //                        'width': '95%'
 //                    })
 
+					scope.init();
+				}
+			}
+		}
+	]
+).directive(
+	'sdk', [
+		function() {
 
+			return {
+				restrict:    'E',
+				require:     '^apisdk',
+				replace:     true,
+				scope:       {},
+				templateUrl: 'views/directives/apisdk/sdk.html',
+				link:        function( scope, elem, attrs ) {
 
-                scope.init();
-            }
-        }
-    }])
-    .directive('sdk', [function() {
+					scope.active = false;
+					scope.id = 'sdk';
+					scope.iframeUrl = null;
 
-        return {
-            restrict: 'E',
-            require: '^apisdk',
-            replace: true,
-            scope: {},
-            templateUrl: 'views/directives/apisdk/sdk.html',
-            link: function(scope, elem, attrs) {
+					scope.init = function() {
 
-                scope.active = false;
-                scope.id = 'sdk';
-                scope.iframeUrl = null;
+						scope.$emit( 'tab:active' );
+						scope.$emit( 'sdk:getServer' );
+					};
 
+					scope.$on(
+						'tab:activate:tab', function( e, tabIdStr ) {
 
-                scope.init = function() {
+							scope.active = scope.id === tabIdStr;
 
-                    scope.$emit('tab:active');
-                    scope.$emit('sdk:getServer');
-                };
+							if ( scope.active && !scope.iframeUrl ) {
+								scope.$emit( 'sdk:getServer' );
+							}
+						}
+					);
 
-                scope.$on('tab:activate:tab', function(e, tabIdStr) {
+					scope.$on(
+						'tab:active:tab', function( e, tabIdStr ) {
 
-                    scope.active = scope.id === tabIdStr;
+							scope.active = scope.id === tabIdStr
 
-                    if (scope.active && !scope.iframeUrl) {
-                        scope.$emit('sdk:getServer');
-                    }
-                });
+						}
+					);
 
-                scope.$on('tab:active:tab', function(e, tabIdStr) {
+					scope.$on(
+						'sdk:getServer:address', function( e, addressStr ) {
 
-                    scope.active = scope.id === tabIdStr
+							scope.iframeUrl = addressStr + '/docs/';
 
-                });
+							angular.element( '#docsFrame' ).attr( 'src', scope.iframeUrl );
+						}
+					);
 
-                scope.$on('sdk:getServer:address', function(e, addressStr) {
-
-                    scope.iframeUrl = addressStr + '/docs/';
-
-                    angular.element('#docsFrame').attr('src', scope.iframeUrl);
-                });
-
-                scope.init();
-            }
-        }
-    }]);
-
+					scope.init();
+				}
+			}
+		}
+	]
+);
