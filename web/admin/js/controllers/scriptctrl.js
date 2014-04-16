821d84bfd543ef24fdcb6702be7741b100f23d2a
Jason Sykes <jasonsykes@dreamfactory.com>
2014-04-16 10:21:08 -0400

    Removing dynamic events

--- web/admin/js/controllers/scriptctrl.js
+++ web/admin/js/controllers/scriptctrl.js
@@ -31,30 +31,30 @@ var ScriptCtrl = function( $scope, Event, Script, Config ) {
 			Event.get( {"all_events": "true"} ).$promise.then(
 				function( response ) {
 					$scope.Events = response.record;
-					$scope.Events.forEach(
-						function( event ) {
-							event.paths.forEach(
-								function( path ) {
-									var preEvent, postEvent, preObj, postObj;
-									var pathIndex = path.path.lastIndexOf( "/" ) + 1;
-									var pathName = path.path.substr( pathIndex );
-									path.verbs.forEach(
-										function( verb ) {
-
-											preEvent = pathName + "." + verb.type + "." + "pre_process";
-											preObj = {"type": verb.type, "event": preEvent, "scripts": []};
-											postEvent = pathName + "." + verb.type + "." + "post_process";
-											postObj = {"type": verb.type, "event": postEvent, "scripts": []};
-											path.verbs.push( preObj );
-											path.verbs.push( postObj );
-										}
-									)
-
-								}
-							)
-
-						}
-					)
+					// $scope.Events.forEach(
+					// 	function( event ) {
+					// 		event.paths.forEach(
+					// 			function( path ) {
+					// 				var preEvent, postEvent, preObj, postObj;
+					// 				var pathIndex = path.path.lastIndexOf( "/" ) + 1;
+					// 				var pathName = path.path.substr( pathIndex );
+					// 				path.verbs.forEach(
+					// 					function( verb ) {
+          //
+					// 						preEvent = pathName + "." + verb.type + "." + "pre_process";
+					// 						preObj = {"type": verb.type, "event": preEvent, "scripts": []};
+					// 						postEvent = pathName + "." + verb.type + "." + "post_process";
+					// 						postObj = {"type": verb.type, "event": postEvent, "scripts": []};
+					// 						path.verbs.push( preObj );
+					// 						path.verbs.push( postObj );
+					// 					}
+					// 				)
+          //
+					// 			}
+					// 		)
+          //
+					// 	}
+					// )
 				}
 			);
 
