499e05c77d9c542cea602d9b613938b9a0326a93
Jason Sykes <jasonsykes@dreamfactory.com>
2014-04-23 13:51:37 -0400

    removing personal info, typo

--- web/admin/js/example.scripts.js
+++ web/admin/js/example.scripts.js
@@ -3,16 +3,17 @@
 //*******Working with System Events********
 
 //	users.list.js
-//	The file contains a script to be run when the event 'users.list' is triggered
+//	The file contains a script to be run when the event 'users.list' is triggered by a
+//  GET on /rest/system/user
 
 var ENABLE_ADD_PROPERTY = true;
 
 _.each(event.data.record, function (record) {
-    record.banned_for_life = (ENABLE_ADD_PROPERTY && (record.email == 'jerryablan@gmail.com'));
+    record.banned_for_life = (ENABLE_ADD_PROPERTY && (record.email == 'scripts@dreamfactory.com'));
     print(record.email + ' is ' + ( record.banned_for_life ? '' : 'not') + ' banned for life.      ');
 });
 
-//apps.create.js  ## this is a post to /rest/app
+//apps.create.js  ## this is a post to /rest/system/app
 //First lets make sure it was just one app sent to be created.
 //We'll do that by seeing if there is a record wrapper object
 if ( event.record ) {
