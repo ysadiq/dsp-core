fa10a6c188d41539b7528dcf58fe9bf620c3ec95
Jerry Ablan <jerryablan@dreamfactory.com>
2014-04-27 15:25:36 -0400

    config file cleanup

--- config/console.php
+++ config/console.php
@@ -23,12 +23,16 @@
  * This is the main configuration file for the DreamFactory Services Platform server application.
  */
 
+$_configFileList = array(
+    'dbConfig'     => array(true, 'database'),
+    'commonConfig' => array(true, 'common'),
+);
+
 /**
- * Load up the database and common configurations between the web and background apps,
+ * Load up the various configurations between the web and background apps,
  * setting globals whilst at it.
  */
-$_dbConfig = require( __DIR__ . '/database.config.php' );
-$_commonConfig = require( __DIR__ . '/common.config.php' );
+extract( Pii::readAppConfig( __DIR__, $_configFileList ) );
 
 //.........................................................................
 //. The configuration himself (like Raab)
@@ -62,7 +66,7 @@ return array(
     /**
      * Preloads
      */
-    'preload'            => array( 'log' ),
+    'preload'            => array('log'),
     /**
      * Imports
      */
@@ -109,24 +113,24 @@ return array(
             'showScriptName' => false,
             'rules'          => array(
                 // REST patterns
-                array( 'rest/get', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'GET' ),
-                array( 'rest/post', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'POST' ),
-                array( 'rest/put', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'PUT' ),
-                array( 'rest/merge', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'PATCH,MERGE' ),
-                array( 'rest/delete', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'DELETE' ),
+                array('rest/get', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'GET'),
+                array('rest/post', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'POST'),
+                array('rest/put', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'PUT'),
+                array('rest/merge', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'PATCH,MERGE'),
+                array('rest/delete', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'DELETE'),
                 // Other controllers
                 '<controller:\w+>/<id:\d+>'              => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>'          => '<controller>/<action>',
                 // fall through to storage services for direct access
-                array( 'admin/<action>', 'pattern' => 'admin/<resource:[_0-9a-zA-Z-]+>/<action>/<id:[_0-9a-zA-Z-\/. ]+>' ),
-                array( 'storage/get', 'pattern' => '<service:[_0-9a-zA-Z-]+>/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'GET' ),
+                array('admin/<action>', 'pattern' => 'admin/<resource:[_0-9a-zA-Z-]+>/<action>/<id:[_0-9a-zA-Z-\/. ]+>'),
+                array('storage/get', 'pattern' => '<service:[_0-9a-zA-Z-]+>/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'GET'),
             ),
         ),
         //	User configuration
         'user'         => array(
             'allowAutoLogin' => true,
-            'loginUrl'       => array( $_defaultController . '/login' ),
+            'loginUrl'       => array($_defaultController . '/login'),
         ),
         'clientScript' => array(
             'scriptMap' => array(
--- config/web.php
+++ config/web.php
@@ -63,14 +63,14 @@ return array(
     /**
      * CORS Configuration
      */
-    'corsWhitelist'      => array( '*' ),
+    'corsWhitelist'      => array('*'),
     'autoAddHeaders'     => true,
     'extendedHeaders'    => true,
     'useResponseObject'  => false,
     /**
      * Preloads
      */
-    'preload'            => array( 'log' ),
+    'preload'            => array('log'),
     /**
      * Imports
      */
@@ -108,24 +108,24 @@ return array(
             'showScriptName' => false,
             'rules'          => array(
                 // REST patterns
-                array( 'rest/get', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'GET' ),
-                array( 'rest/post', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'POST' ),
-                array( 'rest/put', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'PUT' ),
-                array( 'rest/merge', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'PATCH,MERGE' ),
-                array( 'rest/delete', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'DELETE' ),
+                array('rest/get', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'GET'),
+                array('rest/post', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'POST'),
+                array('rest/put', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'PUT'),
+                array('rest/merge', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'PATCH,MERGE'),
+                array('rest/delete', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'DELETE'),
                 // Other controllers
                 '<controller:\w+>/<id:\d+>'              => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>'          => '<controller>/<action>',
                 // fall through to storage services for direct access
-                array( 'admin/<action>', 'pattern' => 'admin/<resource:[_0-9a-zA-Z-]+>/<action>/<id:[_0-9a-zA-Z-\/. ]+>' ),
-                array( 'storage/get', 'pattern' => '<service:[_0-9a-zA-Z-]+>/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'GET' ),
+                array('admin/<action>', 'pattern' => 'admin/<resource:[_0-9a-zA-Z-]+>/<action>/<id:[_0-9a-zA-Z-\/. ]+>'),
+                array('storage/get', 'pattern' => '<service:[_0-9a-zA-Z-]+>/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'GET'),
             ),
         ),
         //	User configuration
         'user'         => array(
             'allowAutoLogin' => true,
-            'loginUrl'       => array( $_defaultController . '/login' ),
+            'loginUrl'       => array($_defaultController . '/login'),
         ),
         'clientScript' => array(
             'scriptMap' => array(
