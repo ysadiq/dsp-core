cbd1044ee6cf2fb104ee41a1d185514fcff80e67
Jerry Ablan <jerryablan@dreamfactory.com>
2014-04-28 15:19:11 -0400

    composer update

--- config/web.php
+++ config/web.php
@@ -25,16 +25,11 @@ use DreamFactory\Platform\Utility\Fabric;
  */
 
 /**
- * Load up the common configurations between the web and background apps,
- * setting globals whilst at it.
- */
-$_commonConfig = require( __DIR__ . '/common.config.php' );
-
-/**
  * Load up the database configuration, free edition, private hosted, or others.
  * Look for non-default database config to override.
  */
 $_dbConfig = array();
+
 if ( file_exists( __DIR__ . '/database.config.php' ) )
 {
     /** @noinspection PhpIncludeInspection */
@@ -44,6 +39,7 @@ else
 {
     if ( Fabric::fabricHosted() )
     {
+        echo 'fabric hosted';
         $_dbConfig = Fabric::initialize();
     }
     else
@@ -58,7 +54,7 @@ else
 
         if ( false !== ( $_host = Fabric::hostedPrivatePlatform( true ) ) )
         {
-            $_dbName = 'hpp_' . str_ireplace( array( '.dreamfactory.com', '-', '.cloud', '.' ), array( null, '_', null, '_' ), $_host );
+            $_dbName = 'hpp_' . str_ireplace( array('.dreamfactory.com', '-', '.cloud', '.'), array(null, '_', null, '_'), $_host );
         }
         else
         {
@@ -78,6 +74,11 @@ else
         );
     }
 }
+/**
+ * Load up the common configurations between the web and background apps,
+ * setting globals whilst at it.
+ */
+$_commonConfig = require( __DIR__ . '/common.config.php' );
 
 //.........................................................................
 //. The configuration
@@ -112,14 +113,14 @@ return array(
     /**
      * CORS Configuration
      */
-    'corsWhitelist'      => array( '*' ),
+    'corsWhitelist'      => array('*'),
     'autoAddHeaders'     => true,
     'extendedHeaders'    => true,
     'useResponseObject'  => false,
     /**
      * Preloads
      */
-    'preload'            => array( 'log' ),
+    'preload'            => array('log'),
     /**
      * Imports
      */
@@ -157,24 +158,24 @@ return array(
             'showScriptName' => false,
             'rules'          => array(
                 // REST patterns
-                array( 'rest/get', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'GET' ),
-                array( 'rest/post', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'POST' ),
-                array( 'rest/put', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'PUT' ),
-                array( 'rest/merge', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'PATCH,MERGE' ),
-                array( 'rest/delete', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'DELETE' ),
+                array('rest/get', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'GET'),
+                array('rest/post', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'POST'),
+                array('rest/put', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'PUT'),
+                array('rest/merge', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'PATCH,MERGE'),
+                array('rest/delete', 'pattern' => 'rest/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'DELETE'),
                 // Other controllers
                 '<controller:\w+>/<id:\d+>'              => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>'          => '<controller>/<action>',
                 // fall through to storage services for direct access
-                array( 'admin/<action>', 'pattern' => 'admin/<resource:[_0-9a-zA-Z-]+>/<action>/<id:[_0-9a-zA-Z-\/. ]+>' ),
-                array( 'storage/get', 'pattern' => '<service:[_0-9a-zA-Z-]+>/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'GET' ),
+                array('admin/<action>', 'pattern' => 'admin/<resource:[_0-9a-zA-Z-]+>/<action>/<id:[_0-9a-zA-Z-\/. ]+>'),
+                array('storage/get', 'pattern' => '<service:[_0-9a-zA-Z-]+>/<path:[_0-9a-zA-Z-\/. ]+>', 'verb' => 'GET'),
             ),
         ),
         //	User configuration
         'user'         => array(
             'allowAutoLogin' => true,
-            'loginUrl'       => array( $_defaultController . '/login' ),
+            'loginUrl'       => array($_defaultController . '/login'),
         ),
         'clientScript' => array(
             'scriptMap' => array(
