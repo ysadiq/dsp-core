a3989e54bc0616d665b6ce312b6c2e9de6b29b93
Jason Sykes <jasonsykes@dreamfactory.com>
2014-06-18 12:14:13 -0400

    fixed pq->pg

--- web/admin/js/controllers/servicectrl.js
+++ web/admin/js/controllers/servicectrl.js
@@ -43,7 +43,7 @@ var ServiceCtrl = function( $scope, Service, $rootScope ) {
         } ,
         {
             name:"PostgreSQL",
-            prefix:"pqsql:"
+            prefix:"pgsql:"
         }];
     Scope.promptForNew = function() {
         Scope.currentServiceId = '';
