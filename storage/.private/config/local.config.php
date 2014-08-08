a776c5c51aadeabc5d61d3af030ea8331490afb6
Jerry Ablan <jerryablan@dreamfactory.com>
2014-08-08 14:49:44 -0400

    cf updates

---
+++ storage/.private/config/local.config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Local configuration file that is never overwritten
+ *
+ * Use to override defaults of system defined settings in /config directory
+ */
+return array(
+//  For example, uncomment below to log all events
+//    'dsp.log_all_events'        => true,
+);
