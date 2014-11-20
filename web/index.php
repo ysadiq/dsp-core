f03c37a615c65b26d91c871c8794c00f7cc78def
Jerry Ablan <jerryablan@dreamfactory.com>
2014-11-20 16:40:32 -0500

    Current work checkpoint. See diff for details.

--- web/index.php
+++ web/index.php
@@ -21,6 +21,7 @@ use DreamFactory\Library\Enterprise\Storage\Enums\EnterprisePaths;
 use DreamFactory\Library\Enterprise\Storage\Resolver;
 use DreamFactory\Library\Utility\AppInstance;
 use DreamFactory\Library\Utility\Environment;
+use DreamFactory\Library\Utility\PlatformInstance;
 use Kisma\Core\Enums\PhpFrameworks;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 
@@ -60,64 +61,59 @@ if ( !class_exists( '\\Yii', false ) )
 }
 
 //  Create the application
-$_app = new AppInstance( _getAppParameters_(), $_autoloader );
-$_app->set( 'autoloader', $_autoloader );
+$_app = new PlatformInstance(
+    call_user_func(
+        function ()
+        {
+            //  Some basics....
+            $_appMode = 'cli' == PHP_SAPI ? 'console' : 'web';
+            $_basePath = dirname( __DIR__ );
+            $_configPath = $_basePath . '/config';
+            $_vendorPath = $_basePath . '/vendor';
+            $_logPath = $_basePath . '/log';
+            $_hostname = Environment::getHostname( true, true );
+            $_hostedInstance = EnterprisePaths::hostedInstance();
+
+            //  Create a resolver
+            $_resolver = new Resolver();
+            $_resolver->setPartitioned( $_hostedInstance );
+            $_resolver->initialize( $_hostname, EnterprisePaths::MOUNT_POINT, $_basePath );
+
+            /** Initialize runtime settings */
+            $_config =
+                array(
+                    /** General Options & Services */
+                    'app.class'                  => 'DreamFactory\\Platform\\Yii\\Components\\Platform' . ucwords( $_appMode ) . 'Application',
+                    'app.mode'                   => $_appMode,
+                    'app.config'                 => null,
+                    'app.resolver'               => $_resolver,
+                    /** Paths */
+                    'app.base_path'              => $_basePath,
+                    'app.config_path'            => $_configPath,
+                    'app.vendor_path'            => $_vendorPath,
+                    'app.log_path'               => $_logPath,
+                    'app.document_root'          => __DIR__,
+                    'app.app_path'               => $_basePath . '/web',
+                    'app.template_path'          => $_configPath . '/templates',
+                    /** Bootstrap Options */
+                    'app.auto_run'               => true,
+                    'app.append_autoloader'      => false,
+                    'app.enable_config_cache'    => true,
+                    'app.framework'              => PhpFrameworks::Yii,
+                    'app.framework.use_yii_lite' => USE_YII_LITE,
+                    'app.hosted_instance'        => $_hostedInstance,
+                    'app.config_file'            => $_configPath . '/' . $_appMode . '.php',
+                    'app.log_file'               => $_appMode . '.' . $_hostname . '.log',
+                    /** Debug Options */
+                    'app.debug'                  => DSP_DEBUG,
+                    'app.debug.use_php_error'    => DSP_DEBUG_PHP_ERROR,
+                );
+
+            return new ParameterBag( $_config );
+        }
+    )
+    , $_autoloader
+);
 
 //  Let 'er rip! This does not return until the request is complete.
 $_app->run( __DIR__ );
-
-//******************************************************************************
-//* Configuration
-//******************************************************************************
-
-/**
- * @return ParameterBag The runtime configuration
- */
-function _getAppParameters_()
-{
-    //  Some basics....
-    $_appMode = 'cli' == PHP_SAPI ? 'console' : 'web';
-    $_basePath = dirname( __DIR__ );
-    $_configPath = $_basePath . '/config';
-    $_vendorPath = $_basePath . '/vendor';
-    $_logPath = $_basePath . '/log';
-    $_hostname = Environment::getHostname( true, true );
-    $_hostedInstance = EnterprisePaths::hostedInstance();
-
-    //  Create a resolver
-    $_resolver = new Resolver();
-    $_resolver->setPartitioned( $_hostedInstance );
-    $_resolver->initialize( $_hostname, EnterprisePaths::MOUNT_POINT, $_basePath );
-
-    /** Initialize runtime settings */
-    $_config =
-        array(
-            /** General Options & Services */
-            'app.class'                  => 'DreamFactory\\Platform\\Yii\\Components\\Platform' . ucwords( $_appMode ) . 'Application',
-            'app.mode'                   => $_appMode,
-            'app.config'                 => null,
-            'app.resolver'               => $_resolver,
-            /** Paths */
-            'app.base_path'              => $_basePath,
-            'app.config_path'            => $_configPath,
-            'app.vendor_path'            => $_vendorPath,
-            'app.log_path'               => $_logPath,
-            'app.document_root'          => __DIR__,
-            'app.app_path'               => $_basePath . '/web',
-            'app.template_path'          => $_configPath . '/templates',
-            /** Bootstrap Options */
-            'app.auto_run'               => true,
-            'app.append_autoloader'      => false,
-            'app.enable_config_cache'    => true,
-            'app.framework'              => PhpFrameworks::Yii,
-            'app.framework.use_yii_lite' => USE_YII_LITE,
-            'app.hosted_instance'        => $_hostedInstance,
-            'app.config_file'            => $_configPath . '/' . $_appMode . '.php',
-            'app.log_file'               => $_appMode . '.' . $_hostname . '.log',
-            /** Debug Options */
-            'app.debug'                  => DSP_DEBUG,
-            'app.debug.use_php_error'    => DSP_DEBUG_PHP_ERROR,
-        );
-
-    return new ParameterBag( $_config );
-}
