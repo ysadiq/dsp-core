ec9947247a7846b54726b319d093a8ae535b15ed
Lee Hicks <leehicks@dreamfactory.com>
2015-01-05 10:53:56 -0500

    remove 'run as admin' config, as it is now unused

--- config/common.config.php
+++ config/common.config.php
@@ -255,8 +255,6 @@ return array_merge(
         'dsp.enable_event_scripts'      => true,
         //  If true, scripts not distributed by DreamFactory will be allowed
         'dsp.enable_user_scripts'       => true,
-        //  If true, scripts will be allowed to use the "run as admin" flag.
-        'dsp.enable_admin_scripts'      => false,
         //  If true, events that have been dispatched to a handler are written to the log
         'dsp.log_events'                => true,
         //  If true, ALL events (with or without handlers) are written to the log.
