5ba09e967ad3d32ca8a257cf9de7b412143f593a
Jerry Ablan <jerryablan@dreamfactory.com>
2015-05-04 16:58:08 -0400

    Current work checkpoint. See diff for details.

--- storage.backup/applications/testapp/config.js
+++
@@ -1,4 +0,0 @@
-var hostUrl = "http://jablan.cloud.dreamfactory.com";
-var dbInfo = {"idField":"id", "dbService":"db", "schemaService":"schema"};
-//var dbInfo = {"idField":"_id", "dbService":"mongo", "schemaService":"mongo"};
-//var dbInfo = {"idField":"id", "dbService":"dynamodb", "schemaService":"dynamodb", "generateIds": true, "idType": "string"};
--- storage.backup/plugins/dreamfactory/jetpack-custom-trigger/README.md
+++
@@ -1,6 +0,0 @@
-# jetpack-custom-trigger
-
-## DreamFactory Services Platform&trade; Custom Trigger Example v0.1.4
-
-This is a DreamFactory Services Platform&trade; plug-in that demonstrates how to create custom resources.
-
